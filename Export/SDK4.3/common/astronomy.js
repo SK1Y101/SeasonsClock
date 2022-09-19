e58de8aff66acaee658fd2f8cd185da5b5cbe409
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2022-09-19 23:15:40 +0100

    return astronomical times in function

--- Export/SDK4.3/common/astronomy.js
+++ Export/SDK4.3/common/astronomy.js
@@ -24,7 +24,9 @@ function DST(now) {
 };
 
 // approximate the time of sunrise, noon, and sunset, given current gps coordinates.
-function sunRise(now, lat, lon, sunAng=-0.83) {
+function sunPos(now, lat, lon) {
+    // https://en.wikipedia.org/wiki/Sunrise_equation
+    // Compute the declination of the sun, and the solar noon time.
     const n = J2000(now);
     const J = n - lon/360;
     const M = (357.5291 + 0.98560028*J)%360;
@@ -32,16 +34,24 @@ function sunRise(now, lat, lon, sunAng=-0.83) {
     const λ = (M+C+180+102.9371) % 360;
     const Jt = 2451545 + J + 0.0053*sin(M) - 0.0069*sin(2*λ);// + now.getTimezoneOffset();
     const dec = asin(sin(λ) * sin(23.44));
-    const w = acos((sin(sunAng) - sin(lat) * sin(dec)) / (cos(lat) * cos(dec)));
-    // Compute the rise and set times
-    const JRise = Jt - w/360;
-    const JSet = Jt + w/360;
-    return [J2Date(JRise), J2Date(Jt), J2Date(JSet)];
+    // Reused values
+    const sinLatDec = sin(lat) * sin(dec);
+    const cosLatDec = cos(lat) * cos(dec);
+    // Compute the hour angle of different points of time.
+    const astroW = acos((sin(-18) - sinLatDec) / cosLatDec);
+    const nauticalW = acos((sin(-12) - sinLatDec) / cosLatDec);
+    const civilW = acos((sin(-6) - sinLatDec) / cosLatDec);
+    const sunriseW = acos((sin(-0.83) - sinLatDec) / cosLatDec);
+    // compute the times for each event and return
+    return {
+        "noon":         J2Date(Jt),
+        "sunrise":      J2Date(Jt - sunriseW/360),
+        "sunset":       J2Date(Jt + sunriseW/360),
+        "civilrise":    J2Date(Jt - civilW/360),
+        "civilset":     J2Date(Jt + civilW/360),
+        "nauticalrise": J2Date(Jt - nauticalW/360),
+        "nauticalset":  J2Date(Jt + nauticalW/360),
+        "astrorise":    J2Date(Jt - astroW/360),
+        "astroset":     J2Date(Jt + astroW/360),
+    };
 };
-
-// const [now, lat, lon] = [new Date(), 50.8768, -0.7867];
-// const [SRise, SNoon, SSet] = sunRise(now, lat, lon);
-// // Update header text
-// console.log(SRise);
-// // Update header text
-// document.querySelector('#header').innerHTML = SRise;
